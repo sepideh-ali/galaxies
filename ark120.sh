0e61f12d543ac8f6545dd46f2ddfe1e9e6c93fc7
Sepideh Alimohammadi <salimohammad@mun.ca>
2024-04-01 21:19:25 -0230

    Added redshift values

--- ark120.sh
+++ ark120.sh
@@ -3,3 +3,7 @@ Galaxy black hole mass = 150e6
 Galaxy brightness variabiliy = Low
 
 Galaxy redshift = 
+ARK 120	0.03271
+GSN 069	0.018
+IRAS 13224-3809	0.066
+RE-J1034-396	0.0424
--- re-j1034-396.sh
+++ re-j1034-396.sh
@@ -2,4 +2,8 @@ Galaxy name = RE J1034 + 396
 Galaxy black hole mass = 2e6
 Galaxy brightness variabiliy = High and periodic
 
-Galaxy redshift =
+Galaxy redshift = 
+ARK 120	0.03271
+GSN 069	0.018
+IRAS 13224-3809	0.066
+RE-J1034-396	0.0424
